882705651db51d6b869e64b0ffdfc2a86c5e38e5
dalkech <julpallec@hotmail.fr>
2026-03-18 17:18:06 +0100

    adding MTT lib with config for converting DTO from cs -> ts

---
+++ Back/Back.WebApi/DTOs/MyDto.cs
@@ -0,0 +1,8 @@
+namespace Back.WebApi.DTOs
+{
+    public class MyDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Age { get; set; }
+    }
+}
---
+++ Back/Back.WebApi/Front/src/app/models/dtos/myDto.ts
@@ -0,0 +1,6 @@
+﻿/* Auto Generated */
+
+export interface MyDto {
+    name: string;
+    age: number;
+}
